a2582b3d5cb5f2821c7cc932c3b4a42791b4ad55
pdupont <pierre.dupont@lyceeastier.com>
2024-11-27 17:27:02 +0100

    2

--- src/fr/btsciel/Ihm.java
+++ src/fr/btsciel/Ihm.java
@@ -3,8 +3,6 @@ package fr.btsciel;
 public class Ihm {
     LesPaysDeLUE gestion = new LesPaysDeLUE();
     public static void main(String[] args) {
-        for (int i = 0; i < tab.length; i++) {
-            System.out.println(tab[i]);
-        }
+    
     }
 }
